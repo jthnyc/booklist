ed316960aa46b9932c3bbaf4934093eb1b8f40de
Joanna Huang <joannathhuang@gmail.com>
2020-08-26 11:48:45 -0400

    [Feat]: adding reducer to dispatch add and remove actions

--- src/components/BookDetails.js
+++ src/components/BookDetails.js
@@ -2,9 +2,15 @@ import React, { useContext } from "react";
 import { BookContext } from "../contexts/BookContext";
 
 const BookDetails = ({ book }) => {
-  const { removeBook } = useContext(BookContext);
+  // const { removeBook } = useContext(BookContext);
+  const { dispatch } = useContext(BookContext);
+
+  const handleClick = () => {
+    dispatch({ type: "REMOVE_BOOK", id: book.id });
+  };
+
   return (
-    <li onClick={() => removeBook(book.id)}>
+    <li onClick={handleClick}>
       <div className="title">{book.title}</div>
       <div className="author">{book.author}</div>
     </li>
